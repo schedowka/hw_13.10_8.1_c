92c3a8dbbcb9d13ef2142c74c34235930d59fd60
schedowka <marina_molodkina@proton.me>
2024-10-18 19:14:05 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int start = 41;
+        int strok = 4;
+        int stolb = 10;
+        for (int i = 0; i < strok; i++) {
+            for (int j = 0; j < stolb; j++) {
+                System.out.print(start + " ");
+                start++;
+
+            }
+            System.out.println();
+        }
+    }
+
+
+    }
